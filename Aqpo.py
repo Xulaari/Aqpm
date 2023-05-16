b8d39fd50c529178e2bae249e6f284e390d2a218
Xula <74150275+Xulaari@users.noreply.github.com>
2023-05-16 21:03:32 +0100

    checks for version relies on cloud yadayada

--- Aqpo.py
+++ Aqpo.py
@@ -1,13 +1,7 @@
-import requests, subprocess
-from tqdm import tqdm
+aqpoversion = 2316
 
-usercontinuation = ""
-
-print(
-    "Aqpo Package Manager [Version 230515]\n©️ 2023 ACHROMATIC LTD. All Rights Reserved.\n"
-)
-
-uinput = input("> Enter the application you want to download or h for help: ").lower()
+import requests, sys, webbrowser
+from tkinter import messagebox
 
 version = requests.get("https://setup.rbxcdn.com/version").text
 
@@ -16,26 +10,53 @@ robloxurl = f"https://setup.rbxcdn.com/{version}-Roblox.exe"
 apps = requests.get("https://pastebin.com/raw/ipRUyBCL").json()
 apps["roblox"] = robloxurl
 
+if (
+    aqpoversion != apps["aqpoversion"]
+    and messagebox.askyesno(
+        title="Aqpo",
+        message=f"This version: {aqpoversion} is out of date. Want to upgrade?",
+    )
+    == True
+):
+    webbrowser.open("https://github.com/Xulaari/Aqpo")
+    sys.exit()
+else:
+    pass
+
+print(
+    "Aqpo Package Manager [Version 230515]\n(c) 2023 ACHROMATIC LTD. All Rights Reserved.\n"
+)
+
+import os, subprocess
+
+usercontinuation = ""
+
+uinput = input(
+    f"Aqpo\\{os.getlogin()}> Enter the application you want to download or h for help: "
+).lower()
+
 if uinput in apps:
     usercontinuation = input(
-        f"> {uinput.capitalize()} selected, Do you want to continue? (y/n): "
+        f"Aqpo\\{os.getlogin()}> {uinput.capitalize()} selected, Do you want to continue? (y/n): "
     )
 else:
     pass
 
 if uinput == "h":
-    print(", ".join(apps))
+    print(", ".join(apps)[13:])
 if uinput in apps and usercontinuation == "y".lower():
-    print(f"> Installing: {uinput.capitalize()}")
+    print(f"Aqpo\\{os.getlogin()}> Installing: {uinput.capitalize()}")
     url = apps[uinput]
     with open(f"{uinput.capitalize()}.exe", "wb") as nv:
         nv.write(requests.get(url).content)
-    wantstopen = input(f"> Do you want to open {uinput.capitalize()}? (y/n): ")
+    wantstopen = input(
+        f"Aqpo\\{os.getlogin()}> Do you want to open {uinput.capitalize()}? (y/n): "
+    )
     if wantstopen == "y":
         subprocess.Popen(f"{uinput.capitalize()}.exe")
     elif wantstopen == "n":
-        print("> Thank you for using the Aqpo package manager!")
+        print("Aqpo\\{os.getlogin()}> Thank you for using the Aqpo package manager!")
     else:
         print("Invalid input.")
 elif usercontinuation == "n".lower():
-    print("> Installation stopped.")
+    print("Aqpo\\{os.getlogin()}> Installation stopped.")
